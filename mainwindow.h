b4cea25bb9deee7108ab66f40192113aeb06a3be
chaconsantiago966-create <chaconsantiago966@gmail.com>
2025-12-06 16:11:52 -0500

    Add MainWindow class definition and header

---
+++ mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "user.h"
+#include "databasemanager.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit MainWindow(const User &u, QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_btnGuardar_clicked();
+    void on_btnFiltrar_clicked();
+    void on_btnExportar_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    User usuario;
+    DatabaseManager *db;
+
+    void cargarTabla();
+};
+
+#endif // MAINWINDOW_H
+
